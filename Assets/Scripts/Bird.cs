f9ae9473fd37fc8bf73280c8813b3a361e0eca4a
PelikandrPC <denis1998zayakin@gmail.com>
2018-05-05 20:03:03 +0300

    бесконечный полет, смерть

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour {
+
+    public Rigidbody2D bird;
+    public float flapHeight;
+	// Use this for initialization
+	void Start () {
+        bird = gameObject.GetComponent(typeof(Rigidbody2D)) as Rigidbody2D;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //bird.velocity = new Vector2(bird.position.x,bird.velocity.y);
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+           if (touch.phase == TouchPhase.Began)
+            {
+                bird.velocity = new Vector2(bird.position.x, flapHeight);
+            }
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            bird.velocity = new Vector2(bird.position.x, flapHeight); 
+        }
+        
+        if(transform.position.y > 5 || transform.position.y < -5)
+        {
+            Death();
+        }
+        
+	}
+
+    public void Death()
+    {
+        bird.velocity = Vector3.zero;
+        transform.position = new Vector2(-5, 0);
+    }
+}
---
+++ Assets/Scripts/Pipe.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pipe : MonoBehaviour
+{
+    Rigidbody2D pipe;
+    Rigidbody2D pipe2;
+    private Bird bird;
+    public float moveSpeed;
+    private int count = 1;
+    Transform p;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        bird = (Bird)FindObjectOfType(typeof(Bird));
+        pipe = gameObject.GetComponent(typeof(Rigidbody2D)) as Rigidbody2D;
+        p = gameObject.GetComponent(typeof(Transform)) as Transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        pipe.velocity = new Vector2(pipe.velocity.y, moveSpeed);
+
+        if (pipe.position.x < -1)
+        {
+            if (count < 2)
+            {
+                float range = Random.Range(-1, 2);
+                Instantiate(pipe, new Vector2(11, range), transform.rotation);
+                count++;
+            }
+
+
+        }
+        if (pipe.position.x < -12)
+        {
+            Destroy(gameObject);
+            count--;
+        }
+
+    }
+    bool CheckDistance(Rigidbody2D pipe)
+    {
+        if (13 - pipe.position.x == 8)
+        {
+            return true;
+        }
+        else
+            return false;
+        /*
+        if (pipe2.position.x - pipe1.position.x == 8)
+        {
+            return true;
+        }
+        else
+            return false; */
+    }
+    void PipeGeneration(Rigidbody2D pipe)
+    {
+        if (13 - pipe.position.x == 8)
+        {
+            if (count < 4)
+            {
+                float range = Random.Range(-3, 0);
+                Instantiate(pipe, new Vector3(11, -2), transform.rotation);
+                count++;
+            }
+
+            if (pipe.position.x < -10)
+            {
+                Destroy(gameObject);
+                count--;
+            }
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            bird.Death();
+        }
+
+    }
+}
