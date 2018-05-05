d4c713398435d5d38f180255956b6fdff669ad30
PelikandrPC <denis1998zayakin@gmail.com>
2018-05-06 01:06:51 +0300

    система набора очков

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -1,16 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Score : MonoBehaviour {
+public class Score : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Text scr;
+    public int points = 0;
+    private int p = 0;
+    Rigidbody2D pipe;
+
+    // Use this for initialization
+    void Start()
+    {
+        scr = gameObject.GetComponent(typeof(Text)) as Text;
+        pipe = gameObject.GetComponent(typeof(Rigidbody2D)) as Rigidbody2D;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if( pipe.position.x<-6)
+        //{
+        //    points++;
+        //}
+        scr.text = " " + p;
+
+        if (transform.position.x < 6)
+        {
+            points++;
+        }
+
+        if ((points % 170 == 0) && (points < 171))
+        {
+            p++;
+        }
+        if ((points > 340) && (points % 180 == 0))
+        {
+            p++;
+        }
+
+
+    }
 }
