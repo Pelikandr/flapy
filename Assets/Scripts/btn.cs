bcdf92160c9f90214da3005653b46e9a501fe4d4
boberOO7 <33721308+boberOO7@users.noreply.github.com>
2018-05-06 00:57:00 +0300

    скрипты для кнопок в меню

---
+++ Assets/Scripts/btn.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class btn : MonoBehaviour {
+    public GameObject settings;
+
+    public void NewGame()
+    {
+#pragma warning disable CS0618 // Type or member is obsolete
+        Application.LoadLevel(1);
+#pragma warning restore CS0618 // Type or member is obsolete
+     // GameObject.DontDestroyOnLoad(gameObject);
+    }
+
+    public void Settings()
+    {
+        settings.SetActive(!settings.activeSelf);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void setMusic(float value)
+    {
+        Global.music = value;
+    }
+
+    public void setSound(float value)
+    {
+        Global.sound = value;
+    }
+}
