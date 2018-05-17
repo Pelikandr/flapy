a7f134dc290519293977960d3e69c1510cf58547
boberOO7 <33721308+boberOO7@users.noreply.github.com>
2018-05-17 09:50:34 +0300

    музыка, игровые настройки музыки

---
+++ Assets/Scripts/GameSettings.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class GameSettings : MonoBehaviour {
+
+    public Slider slider;
+    public Text valueCount;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        valueCount.text = slider.value.ToString();
+        AudioListener.volume = slider.value;
+	}
+}
